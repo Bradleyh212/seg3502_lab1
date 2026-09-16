f01a5957aa97e07a41a48dbc89dd6b28ba3250b1
Mathew Al-Frenn <malfr102@uottawa.ca>
2026-09-16 18:12:07 -0400

    init calc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TempConverter } from './temp-converter/temp-converter';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
-  imports: [TempConverter]
+  imports: [TempConverter, CalculatorComponent]
 })
 export class App {
   title = 'lab1-temp-converter';
