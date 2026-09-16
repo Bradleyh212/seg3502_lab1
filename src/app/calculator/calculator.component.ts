6db80dc25133695b21ec201a1a65a1c433cf7c43
Bradley-Hans Desmornes <145512746+Bradleyh212@users.noreply.github.com>
2026-09-16 18:20:12 -0400

    added ts

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './calculator.component.html',
 })
 export class CalculatorComponent {
+  result = 0;
+
+  add(firstNumber: string, secondNumber: string): void {
+    this.result = Number(firstNumber) + Number(secondNumber);
+  }
+
+  subtract(firstNumber: string, secondNumber: string): void {
+    this.result = Number(firstNumber) - Number(secondNumber);
+  }
+
+  multiply(firstNumber: string, secondNumber: string): void {
+    this.result = Number(firstNumber) * Number(secondNumber);
+  }
+
+divide(firstNumber: string, secondNumber: string): void {
+  if (Number(secondNumber) === 0) {
+    this.result = 0;
+  } else {
+    this.result = Number(firstNumber) / Number(secondNumber);
+  }
+}
+
 }
