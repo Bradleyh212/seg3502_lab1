a283157201c46492ae9f583453c51ac3d0872627
Mathew Al-Frenn <malfr102@uottawa.ca>
2026-09-16 18:05:14 -0400

    added folder for calc

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-calculator',
+  styleUrl: './calculator.component.css',
+  templateUrl: './calculator.component.html',
+})
+export class CalculatorComponent {
+}
